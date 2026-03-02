271733fc69687405c04ee17cd1f77bdbbb1fd101
bnckab5 <bkabariya211@rku.ac.in>
2026-03-02 21:05:18 +0530

    UI Design

---
+++ Controllers/UserController.cs
@@ -0,0 +1,23 @@
+﻿using Microsoft.AspNetCore.Mvc;
+
+namespace RecipeFinder_WebApp.Controllers
+{
+    public class UserController : Controller
+    {
+        // This action will handle your Home Page
+        public IActionResult Index()
+        {
+            return View();
+        }
+
+        // You can add your other actions here
+        public IActionResult About()
+        {
+            return View();
+        }
+        public IActionResult Contact()
+        {
+            return View();
+        }
+    }
+}
